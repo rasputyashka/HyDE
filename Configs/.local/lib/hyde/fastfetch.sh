0ded27af9de630d5e5ee131672ffa2c11c4fb39f
Konstantin Rasputin <rasputyashka@gmail.com>
2025-06-02 14:19:34 +0300

    refactor: update fastfetch.sh

--- Configs/.local/lib/hyde/fastfetch.sh
+++ Configs/.local/lib/hyde/fastfetch.sh
@@ -9,10 +9,10 @@ fi
 
 USAGE() {
   cat <<USAGE
-Usage: fastfetch [commands] [options]
+Usage: fastfetch.sh [commands] [options]
 
 commands:
-  logo    Display a random logo
+  logo    Returns path to the logo file.
 
 options:
   -h, --help,     Display command's help message
@@ -32,23 +32,29 @@ iconDir="${XDG_DATA_HOME:-$HOME/.local/share}/icons"
 image_dirs=()
 hyde_distro_logo=${iconDir}/Wallbash-Icon/distro/$LOGO
 
+function print_images() {
+  find -L "${image_dirs[@]}" -maxdepth 1 -type f \( -name "wall.quad" -o -name "wall.sqre" -o -name "*.icon" -o -name "*logo*" -o -name "*.png" \) ! -path "*/wall.set*" ! -path "*/wallpapers/*.png" 2>/dev/null
+
+}
+
 # Parse the main command
 case $1 in
 logo) # eats around 13 ms
   random() {
     (
       image_dirs+=("${confDir}/fastfetch/logo")
-      image_dirs+=("${iconDir}/Wallbash-Icon/fastfetch/")
+      image_dirs+=("${iconDir}/Wallbash-Icon/fastfetch/") # wallbash logos
+      # some themes has their own logo. Catppuccin Latte, for example.
       if [ -n "${HYDE_THEME}" ] && [ -d "${confDir}/hyde/themes/${HYDE_THEME}/logo" ]; then
         image_dirs+=("${confDir}/hyde/themes/${HYDE_THEME}/logo")
       fi
-      # [ -d "$HYDE_CACHE_HOME" ] && image_dirs+=("$HYDE_CACHE_HOME")
-      [ -f "$hyde_distro_logo" ] && echo "${hyde_distro_logo}"
-      image_dirs+=("$HYDE_CACHE_HOME/wall.quad")
-      image_dirs+=("$HYDE_CACHE_HOME/wall.sqre")
+      [ -d "$HYDE_CACHE_HOME" ] && image_dirs+=("$HYDE_CACHE_HOME")
+      [ -f "$hyde_distro_logo" ] && echo "${hyde_distro_logo}" # displays distro logo
       [ -f "$HOME/.face.icon" ] && image_dirs+=("$HOME/.face.icon")
-      # also .bash_logout may be matched with this find
-      find -L "${image_dirs[@]}" -maxdepth 1 -type f \( -name "wall.quad" -o -name "wall.sqre" -o -name "*.icon" -o -name "*logo*" -o -name "*.png" \) ! -path "*/wall.set*" ! -path "*/wallpapers/*.png" 2>/dev/null
+      # .bash_logout matches *logo* in case image_dirs is an empty list
+      if [[ ${image_dirs[@]} ]]; then
+        print_images
+      fi
     ) | shuf -n 1
   }
   help() {
@@ -106,7 +112,7 @@ HELP
         ;;
       esac
     done
-    find -L "${image_dirs[@]}" -maxdepth 1 -type f \( -name "wall.quad" -o -name "wall.sqre" -o -name "*.icon" -o -name "*logo*" -o -name "*.png" \) ! -path "*/wall.set*" ! -path "*/wallpapers/*.png" 2>/dev/null
+    print_images
   ) | shuf -n 1
 
   ;;
